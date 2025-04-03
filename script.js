156bedab1064526c16f4b5a10bc53ea10226851d
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-04-03 21:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //your JS code here. If required.
 function printsNum(){
-	let res = "";
+	let result = "";
 	for(let i = 1 ; i<= 20; i++){
 		if(i %3  === 0 && i % 5  === 0){
 			result += "FizzBuzz\n"; 
 		} else if(i %3 === 0){
-			result += "Fizz\n
+			result += "Fizz\n";
 		} else if (i % 5  === 0){
-  result += "Buzz\n
+     result += "Buzz\n"
 		}else {
 			result += i + "\n";
 		}
 		
 	}
-	alert(result); 
+	window.alert(result); 
 }
 printsNum();
